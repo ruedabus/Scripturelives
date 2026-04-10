a07360a5c0a78da2aa033531411099c9c45298d0
Steven <ruedabus1@yahoo.com>
2026-04-10 15:29:34 -0400

    Add Faith Tails YouTube channel link to sidebar footer

--- src/components/BibleReader.tsx
+++ src/components/BibleReader.tsx
@@ -753,6 +753,23 @@ export default function BibleReader() {
 
           {/* Sidebar footer links */}
           <div className="shrink-0 border-t border-stone-800 px-4 py-4 space-y-1">
+
+            {/* YouTube channel */}
+            <a
+              href="https://www.youtube.com/@FaithTails"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2.5 rounded-lg px-2 py-2 text-xs font-medium text-stone-300 hover:bg-stone-800 hover:text-white transition group"
+            >
+              {/* YouTube brandmark SVG */}
+              <span className="shrink-0 flex items-center justify-center w-6 h-6 rounded bg-[#FF0000] group-hover:bg-[#cc0000] transition">
+                <svg viewBox="0 0 24 24" className="w-3.5 h-3.5 fill-white" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+                </svg>
+              </span>
+              <span>Faith Tails on YouTube</span>
+            </a>
+
             <a href="/donate" className="flex items-center gap-2 rounded-lg px-2 py-1.5 text-xs text-amber-400 hover:bg-stone-800 transition">
               <HandCoins size={14} /> Support Scripture Lives
             </a>
