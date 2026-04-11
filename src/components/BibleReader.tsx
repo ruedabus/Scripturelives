407f782f2841f7e812781f05e668f6d7518c88ee
Steven <ruedabus1@yahoo.com>
2026-04-10 21:53:18 -0400

    Fix Faith Tails avatar display for transparent PNG

--- src/components/BibleReader.tsx
+++ src/components/BibleReader.tsx
@@ -766,7 +766,7 @@ export default function BibleReader() {
               <img
                 src="/FT_transparent_logo.png"
                 alt="Faith Tails"
-                className="shrink-0 w-6 h-6 rounded-full object-cover ring-1 ring-stone-600 group-hover:ring-amber-400 transition"
+                className="shrink-0 w-7 h-7 rounded-full object-contain bg-white p-0.5 ring-1 ring-stone-600 group-hover:ring-amber-400 transition"
               />
               <span>Faith Tails on YouTube</span>
             </a>
