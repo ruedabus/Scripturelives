68dc8d9f36c4e1537a9cc328e1f1190260e32b25
Steven <ruedabus1@yahoo.com>
2026-05-30 10:25:10 -0400

    mobile navigation fix2

--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -1,1 +1,10 @@
-export { default } from "@/components/BibleReader";
+import { Suspense } from "react";
+import BibleReader from "@/components/BibleReader";
+
+export default function ReadPage() {
+  return (
+    <Suspense fallback={null}>
+      <BibleReader />
+    </Suspense>
+  );
+}
